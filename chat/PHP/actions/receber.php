16d43e67e3239687250c720171b53a4282b923c2
Perera <Perera@gmail>
2023-09-03 22:38:27 -0300

    display inverso das msgs

--- chat/PHP/actions/receber.php
+++ chat/PHP/actions/receber.php
@@ -6,13 +6,17 @@ $sql = "SELECT nome, msg, datahora FROM `chat`";
 
 $msgs = $conexao->query($sql);
 
-foreach ($msgs as $msg) {
+$array_msg = array();
+
+$i = 0;
+
+foreach($msgs as $msg){
     if($msg['nome']==$user){$me = "me";}else{$me = "";};
-    echo "<div class='caixa_msg " . $me . "'>";
-    echo "<div class='rotulo'>";
-    echo "<p>" . $msg['nome'] . "</p>";
-    echo "<p>" . $msg['datahora'] . "</p>";
-    echo "</div>";
-    echo "<div class='mensagem'>" . $msg['msg'] . "</div>";
-    echo "</div>";
+    $array_msg[$i++] = "<div class='caixa_msg " . $me . "'>" . "<div class='rotulo'>" . "<p>" . $msg['nome'] . "</p>" . "<p>" . $msg['datahora'] . "</p>" . "</div>" . "<div class='mensagem'>" . $msg['msg'] . "</div>" . "</div>";
+}
+
+$array_msg = array_reverse($array_msg);
+
+foreach($array_msg as $arr){
+    echo $arr;
 }
