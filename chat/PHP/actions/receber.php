4a4e8ceb4ccd57620d4bdd4bd76b85f85f4f2d15
Perera <perera@gmail.com>
2023-09-06 14:44:36 -0300

    deletar mensagem

--- chat/PHP/actions/receber.php
+++ chat/PHP/actions/receber.php
@@ -2,7 +2,7 @@
 
 require_once('./PHP/includes/conexao.php');
 
-$sql = "SELECT nome, msg, datahora FROM `chat`";
+$sql = "SELECT id, nome, msg, datahora FROM `chat`";
 
 $msgs = $conexao->query($sql);
 
@@ -12,7 +12,7 @@ $i = 0;
 
 foreach($msgs as $msg){
     if($msg['nome']==$user){$me = "me";}else{$me = "";};
-    $array_msg[$i++] = "<div class='caixa_msg " . $me . "'>" . "<div class='rotulo'>" . "<p>" . $msg['nome'] . "</p>" . "<p>" . $msg['datahora'] . "</p>" . "</div>" . "<div class='mensagem'>" . $msg['msg'] . "</div>" . "</div>";
+    $array_msg[$i++] = "<div class='caixa_msg " . $me . "'>" . "<div class='rotulo'>" . "<p>" . $msg['nome'] . "</p>" . "<p>" . $msg['datahora'] . "</p>" . "<a href='./actions/deletar_msg.php?id=" . $msg['id'] . "'>'><img src='./images/lixo.png' alt='apagar'></a>" . "</div>" . "<div class='mensagem'>" . $msg['msg'] . "</div>" . "</div>";
 }
 
 $array_msg = array_reverse($array_msg);
