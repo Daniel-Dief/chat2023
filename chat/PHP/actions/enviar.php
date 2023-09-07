7c1b151eb801acbc4ae99ccbef0c72bdca862fa4
Perera <Perera@gmail>
2023-09-07 18:34:19 -0300

    perfil no BD

--- chat/PHP/actions/enviar.php
+++ chat/PHP/actions/enviar.php
@@ -3,11 +3,12 @@
 require_once('../includes/conexao.php');
 
 $nome = filter_input(INPUT_GET, 'user', FILTER_SANITIZE_SPECIAL_CHARS);
+$perfil = filter_input(INPUT_GET, 'perfil', FILTER_SANITIZE_SPECIAL_CHARS);
 $msg = filter_input(INPUT_GET, 'msg', FILTER_SANITIZE_SPECIAL_CHARS);
 $datahora = filter_input(INPUT_GET, 'datetime', FILTER_SANITIZE_SPECIAL_CHARS);
 
 if(!empty($msg) && !empty($nome)){
-    $sql = "INSERT INTO `chat` (`nome`, `msg`, `datahora`) VALUES ('$nome', '$msg', '$datahora')";
+    $sql = "INSERT INTO `chat` (`nome`, `perfil`, `msg`, `datahora`) VALUES ('$nome', '$perfil', '$msg', '$datahora')";
     $conexao->query($sql);
 }
 
