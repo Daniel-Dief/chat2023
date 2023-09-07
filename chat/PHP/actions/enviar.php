6bf6326daf466a7f23d711d783009c49d22cdec2
Perera <Perera@gmail>
2023-09-06 21:03:22 -0300

    arrumando horario

--- chat/PHP/actions/enviar.php
+++ chat/PHP/actions/enviar.php
@@ -4,9 +4,10 @@ require_once('../includes/conexao.php');
 
 $nome = filter_input(INPUT_GET, 'user', FILTER_SANITIZE_SPECIAL_CHARS);
 $msg = filter_input(INPUT_GET, 'msg', FILTER_SANITIZE_SPECIAL_CHARS);
+$datahora = filter_input(INPUT_GET, 'datetime', FILTER_SANITIZE_SPECIAL_CHARS);
 
 if(!empty($msg) && !empty($nome)){
-    $sql = "INSERT INTO `chat` (`nome`, `msg`) VALUES ('$nome', '$msg')";
+    $sql = "INSERT INTO `chat` (`nome`, `msg`, `datahora`) VALUES ('$nome', '$msg', '$datahora')";
     $conexao->query($sql);
 }
 
