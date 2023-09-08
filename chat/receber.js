cb751f4406190d9c16549d0e6c253a7614086514
Perera <Perera@gmail>
2023-09-07 22:35:50 -0300

    preparando para exibir

--- chat/receber.js
+++ chat/receber.js
@@ -5,10 +5,33 @@ async function getMsgs(){
         return data.json();
     });
     json.forEach(line => {
-        console.log(line['id']);
-        console.log(line['nome']);
-        console.log(line['perfil']);
-        console.log(line['msg']);
-        console.log(line['datahora']);
+
     });
 }
+
+/*
+outros 👇
+
+<div class='caixa_msg'>
+	<div class='rotulo'>
+		<p class='nome-user'>User</p>
+		<p>2023/08/06 22:26:00</p>
+	</div>
+	<div class='mensagem'>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam deserunt vero, officiis ipsum esse, laborum quidem nobis consectetur repellendus vel, quaerat aut sint odio sapiente neque soluta! Ratione, voluptatem quod.
+	</div>
+</div>
+
+meu 👇
+
+<div class='caixa_msg me'>
+	<div class='rotulo'>
+		<p class='nome-user'>User</p>
+		<p>2023/08/06 22:26:00</p>
+        <a href='./PHP/actions/deletar_msg.php?id='${id}'><img src='./images/lixo.png' alt='apagar'></a>
+	</div>
+	<div class='mensagem'>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam deserunt vero, officiis ipsum esse, laborum quidem nobis consectetur repellendus vel, quaerat aut sint odio sapiente neque soluta! Ratione, voluptatem quod.
+	</div>
+</div>
+*/
